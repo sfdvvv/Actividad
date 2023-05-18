e4ecd1b527c5a0ea10a3d38cad80427c6bd85981
sfdvvv <132546489+sfdvvv@users.noreply.github.com>
2023-05-18 09:13:36 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    led.setBrightness(led.brightness() + receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(-25)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("POWER")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "POWER") {
+        encendido = !(encendido)
+        led.setBrightness(125)
+        led.enable(encendido)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(25)
+})
+let encendido = false
+radio.setGroup(1)
+basic.forever(function () {
+    radio.setGroup(1)
+    encendido = false
+    led.enable(encendido)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
